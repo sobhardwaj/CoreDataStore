87c07db9dcd52c31abfe22b3bcdcdf86731b4aff
StuartShay <sshay@yahoo.com>
2016-03-25 17:39:54 -0400

    Update

---
+++ src/CoreDataStore.Domain/Entities/DataEventRecord.cs
@@ -0,0 +1,18 @@
+﻿namespace CoreDataStore.Domain.Entities
+{
+    using System;
+
+    using Newtonsoft.Json;
+
+    // >dnx . ef migration add testMigration
+
+    public class DataEventRecord
+    {
+        public long DataEventRecordId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Timestamp { get; set; }
+        public SourceInfo SourceInfo { get; set; }
+        public int SourceInfoId { get; set; }
+    }
+}
---
+++ src/CoreDataStore.Domain/Interfaces/IDataAccessProvider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using CoreDataStore.Domain.Entities;
+
+namespace CoreDataStore.Domain.Interfaces
+{
+    public interface IDataAccessProvider
+    {
+        void AddDataEventRecord(DataEventRecord dataEventRecord);
+
+        void UpdateDataEventRecord(long dataEventRecordId, DataEventRecord dataEventRecord);
+
+        void DeleteDataEventRecord(long dataEventRecordId);
+
+        DataEventRecord GetDataEventRecord(long dataEventRecordId);
+
+        List<DataEventRecord> GetDataEventRecords();
+
+        List<SourceInfo> GetSourceInfos(bool withChildren);
+    }
+}
