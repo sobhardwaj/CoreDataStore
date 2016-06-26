489f9bef89313874add4153db753e0aeb6db0174
Stuart Shay <sshay@yahoo.com>
2016-06-26 13:57:42 -0400

    Added Commands

--- src/CoreDataStore.Web/prod.cmd
+++ src/CoreDataStore.Web/prod.cmd
@@ -1,3 +1,4 @@
 set ASPNETCORE_ENVIRONMENT=Production
+set ASPNETCORE_URLS=http://*:5002
 dotnet run
 pause
---
+++ src/CoreDataStore.Web/stage.cmd
@@ -0,0 +1,4 @@
+set ASPNETCORE_ENVIRONMENT=Staging
+set ASPNETCORE_URLS=http://*:5001
+dotnet run
+pause
