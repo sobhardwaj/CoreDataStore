33b37ba5bcf14ba9e8911fcb98ccffee0f32c943
Stuart Shay <sshay@yahoo.com>
2016-07-19 23:53:34 -0400

    Update

--- test/CoreDataStore.Data.SqlServer.Test/Repositories/LPCReportRepositoryTest.cs
+++ test/CoreDataStore.Data.SqlServer.Test/Repositories/LPCReportRepositoryTest.cs
@@ -94,16 +94,10 @@ namespace CoreDataStore.Data.SqlServer.Test.Repositories
         }
 
 
-        [Fact]
+        [Fact()]
         public void PassingTest()
         {
             Assert.Equal(4, Add(2, 2));
-        }
-
-        [Fact(Skip = "ci test")]
-        public void FailingTest()
-        {
-            Assert.Equal(5, Add(2, 2));
         }
 
         int Add(int x, int y)
