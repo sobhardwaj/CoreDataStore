0b1db29ed1eebf574b368ae9f92d0630b4ac677c
Le Duong <me@leduong.com>
2016-08-05 08:47:08 +0700

    dynamic api

--- src/CoreDataStore.Web/src/app/appsettings.ts
+++ src/CoreDataStore.Web/src/app/appsettings.ts
@@ -1,4 +1,6 @@
 export class AppSettings {
   public static get ApiEndpoint(): string {
-    return 'http://localhost:5000/api/'; }
+    // return 'http://localhost:5000/api/'; 
+    return 'http://' + window.location.hostname + ':5000/api/';
+  }
 }
