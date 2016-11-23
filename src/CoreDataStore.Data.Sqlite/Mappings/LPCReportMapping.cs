e3dcdfc97e588ee0cf035645cc250e32511e3c78
Stuart Shay <sshay@yahoo.com>
2016-11-22 21:08:45 -0500

    Update

--- src/CoreDataStore.Data.Sqlite/Mappings/LPCReportMapping.cs
+++ src/CoreDataStore.Data.Sqlite/Mappings/LPCReportMapping.cs
@@ -6,13 +6,10 @@ namespace CoreDataStore.Data.Sqlite.Mappings
 {
     public class LPCReportMapping : EntityMappingConfiguration<LPCReport>
     {
-        //https://github.com/aspnet/EntityFramework/issues/2805
-
         public override void Map(EntityTypeBuilder<LPCReport> b)
         {
             b.Property(p => p.Name).HasMaxLength(200);
             b.Property(p => p.Architect).HasMaxLength(200);
-
         }
     }
 }
