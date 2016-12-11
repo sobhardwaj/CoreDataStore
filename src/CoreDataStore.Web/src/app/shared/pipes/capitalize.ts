19b0b4555d0be0d5b15ed42c4547d1a204a6e6e4
Le Duong <me@leduong.com>
2016-12-12 00:38:46 +0700

    bug

--- src/CoreDataStore.Web/src/app/shared/pipes/capitalize.ts
+++ src/CoreDataStore.Web/src/app/shared/pipes/capitalize.ts
@@ -4,6 +4,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class CapitalizePipe implements PipeTransform {
 
   transform(input: string): string {
-    return (input === '') ? input : (input.charAt(0).toUpperCase() + input.slice(1));
+    return (!input || input === '') ? input : (input.charAt(0).toUpperCase() + input.slice(1));
   }
 }
