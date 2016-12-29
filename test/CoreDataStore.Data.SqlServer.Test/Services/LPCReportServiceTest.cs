afe0704ef4df6d40eb9c4106a3cbd6ff66e1b038
Stuart Shay <sshay@yahoo.com>
2016-12-29 18:14:12 -0500

    Cleanup

--- test/CoreDataStore.Data.SqlServer.Test/Services/LPCReportServiceTest.cs
+++ test/CoreDataStore.Data.SqlServer.Test/Services/LPCReportServiceTest.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.IO;
 using CoreDataStore.Data.Filters;
 using CoreDataStore.Data.Interfaces;
--- test/CoreDataStore.Data.SqlServer.Test/Services/LandmarkServiceTest.cs
+++ test/CoreDataStore.Data.SqlServer.Test/Services/LandmarkServiceTest.cs
@@ -1,16 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO;
+using CoreDataStore.Data.Interfaces;
+using CoreDataStore.Data.SqlServer.Repositories;
+using CoreDataStore.Service.Interfaces;
+using CoreDataStore.Service.Mappings;
+using CoreDataStore.Service.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace CoreDataStore.Data.SqlServer.Test.Services
 {
 
     public class LandmarkServiceTest
     {
+        private readonly ILandmarkService _landmarkService;
+
         public LandmarkServiceTest()
         {
-            
+            var builder = new ConfigurationBuilder()
+             .SetBasePath(Directory.GetCurrentDirectory())
+             .AddJsonFile("appsettings.json")
+             .Build();
+
+            var dbonnection = builder.GetConnectionString("SqlServer");
+
+            var serviceProvider = new ServiceCollection()
+                .AddDbContext<NYCLandmarkContext>(options => options.UseSqlServer(dbonnection))
+                .AddScoped<ILandmarkRepository, LandmarkRepository>()
+                .AddScoped<ILandmarkService, LandmarkService>()
+                .BuildServiceProvider();
+
+            serviceProvider.GetRequiredService<NYCLandmarkContext>();
+            serviceProvider.GetRequiredService<ILandmarkRepository>();
+
+            _landmarkService = serviceProvider.GetRequiredService<ILandmarkService>();
+
+            AutoMapperConfiguration.Configure();
         }
 
 
