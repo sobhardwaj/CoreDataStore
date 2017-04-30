e4a7ddc4c80236d3afb4d6f492da125e5eb84b2f
Le Duong <me@leduong.com>
2017-04-30 08:50:02 +0000

    add new branch ng4

--- src/CoreDataStore.Web/bs-config.js
+++ src/CoreDataStore.Web/bs-config.js
@@ -1,18 +1,20 @@
 var proxyMiddleware = require('http-proxy-middleware');
 var fallbackMiddleware = require('connect-history-api-fallback');
-
+var port = process.env.PORT || 8000;
+var ApiEndpoint = process.env.APIENDPOINT || 'localhost';
+var PortEndpoint = process.env.PORTENDPOINT || 5000;
 module.exports = {
-    port: 8000,
+    port: port,
     files: ["wwwroot/**/*.{html,htm,css,js}"],
     server: {
         baseDir: "wwwroot",
         middleware: {
             1: proxyMiddleware('/api', {
-                target: 'http://localhost:5000',
+                target: ['http://',ApiEndpoint,':',PortEndpoint].join(''),
                 changeOrigin: false   
             }),       
             2: proxyMiddleware('/swagger', {
-                target: 'http://localhost:5000',
+                target: ['http://',ApiEndpoint,':',PortEndpoint].join(''),
                 changeOrigin: false   
             })           
         }
