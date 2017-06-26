9b48bd2a8b1b74a349e500696df3207dd6bbabed
Le Duong <me@leduong.com>
2017-06-26 07:36:14 +0700

    selenium-webdriver  only work node 6,7

--- selenium.test.js
+++ selenium.test.js
@@ -2,17 +2,17 @@ var username = process.env.SAUCE_USERNAME || '';
 var accessKey = process.env.SAUCE_ACCESS_KEY || '';
 var TRAVIS_JOB_NUMBER = process.env.TRAVIS_JOB_NUMBER;
 
-var webdriver = require('selenium-webdriver'),
-  urlSaucelabs,
-  driver;
+const { Builder, By, until } = require('selenium-webdriver');
+var urlSaucelabs, browser;
 
 if (username && accessKey) {
   urlSaucelabs = ["http://", username, ":", accessKey, "@ondemand.saucelabs.com:80/wd/hub"].join('');
-  driver = new webdriver.Builder().
+  browser = new Builder().
   withCapabilities({
-    'browserName': 'googlechrome',
-    'platform': 'Windows 7',
-    'version': '59.0.',
+    'browserName': 'safari',
+    'platform': 'iOS 10.2',
+    'version': '10.0.',
+    'device': 'iPad Simulator',
     'username': username,
     'accessKey': accessKey,
     'tunnelIdentifier': TRAVIS_JOB_NUMBER
@@ -20,11 +20,40 @@ if (username && accessKey) {
   usingServer(urlSaucelabs).
   build();
 
-  driver.get("http://127.0.0.1:3000/#/diagnostics");
+  browser.get("http://127.0.0.1:3000/#/diagnostics");
 
-  driver.getTitle().then(function(title) {
-    console.log("title is: " + title);
+  browser.wait(findLink('#/references'), 2000).then(clickLink);
+
+  browser.wait(findLink('#/diagnostics'), 2000).
+  then(clickLink).
+  then(logTitle).
+  then(closeBrowser, handleFailure);
+
+}
+
+function logTitle() {
+  browser.getTitle().then(function(title) {
+    console.log('Current Page Title: ' + title);
   });
+}
 
-  driver.quit();
+
+function clickLink(link) {
+  link.click();
+}
+
+function findLink(link) {
+  link = ['[href="', link, '"]'].join('');
+  return browser.findElements(By.css(link)).then((res) => {
+    return res[0];
+  });
+}
+
+function closeBrowser() {
+  browser.quit();
+}
+
+function handleFailure(err) {
+  console.error('Something went wrong\n', err.stack, '\n');
+  closeBrowser();
 }
