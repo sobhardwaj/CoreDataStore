63f487406290e1fa0ec806a530ddeead67c4f259
dpk <d.p.karpov@gmail.com>
2017-08-11 22:15:45 +0300

    update

---
+++ travis_build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ `npm -v` != 3* ]]; then npm i -g npm@3; fi
+cd src/CoreDataStore.Web
+npm run clean
+npm run build
+
+rm -f wwwroot/index.html
+cd ../..
+docker build -f docker/node.dockerfile -t stuartshay/coredatastore:node$TRAVIS_NODE_VERSION-$TRAVIS_BUILD_NUMBER .
+docker run --env-file docker/env.staging -d -p 3000:3000 stuartshay/coredatastore:node$TRAVIS_NODE_VERSION-$TRAVIS_BUILD_NUMBER
+docker ps -a
+
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+docker push stuartshay/coredatastore:node$TRAVIS_NODE_VERSION-$TRAVIS_BUILD_NUMBER
