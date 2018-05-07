1c995f2a649adbe11b923007a181fa545f5f58e6
Stuart Shay <sshay@yahoo.com>
2018-05-07 17:30:13 -0400

    Update Packages

---
+++ setup-env.bat
@@ -0,0 +1,21 @@
+@echo off
+
+set NG_ENVIRONMENT=Dev
+set LANDMARK=https://api.coredatastore.com/api/
+set MAPSAPI=https://api-maps.navigatorglass.com/api/
+set LOCATIONAPI=https://api-location.coredatastore.com/api/
+
+@echo on
+@echo. 
+
+@echo ****** ENV Variables ******
+@echo NG_ENVIRONMENT - %NG_ENVIRONMENT%
+@echo. 
+
+@echo ****** ENV Variables ******
+@echo LANDMARK= - %LANDMARK%
+@echo MAPSAPI - %MAPSAPI%
+@echo LOCATIONAPI - %LOCATIONAPI%
+@echo. 
+
+
